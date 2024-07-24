757d93d205510b34bb4667f51c872f4b8c32ca37
won <andrew112517@naver.com>
2024-07-24 23:00:34 +0900

    [Feat]tableView-BannerCell추가

--- BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/CalendarViewController.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/CalendarViewController.swift
@@ -10,6 +10,10 @@ import SnapKit
 
 final class CalendarViewController: UIViewController {
     // MARK: - Properties
+//    private let heightBetweenCells: CGFloat = 12
+//    private let heightOfCell: CGFloat = 72
+    
+    // MARK: - UI Components
     lazy var tableView = UITableView()
 
     // MARK: - Life Cycle ⭐️
@@ -42,6 +46,9 @@ final class CalendarViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
+        // 셀 등록
+        tableView.register(BannerCell.self, forCellReuseIdentifier: BannerCell.identifier)
+        
         self.view.addSubview(tableView)
         
         // 제약조건
@@ -59,15 +66,26 @@ extension CalendarViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.backgroundColor = .red
-        return cell
+        let tempCell = UITableViewCell()
+        tempCell.backgroundColor = .gray
+        
+        if indexPath.row == 0 {
+            let bannerCell = tableView.dequeueReusableCell(withIdentifier: BannerCell.identifier, for: indexPath) as! BannerCell
+            return bannerCell
+        }
+        
+        return tempCell
+        
     }
 }
 
 // MARK: - UITableView Delegate
 extension CalendarViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        if indexPath.row == 0 {
+            return 127
+        }
+        
+        return 100
     }
 }

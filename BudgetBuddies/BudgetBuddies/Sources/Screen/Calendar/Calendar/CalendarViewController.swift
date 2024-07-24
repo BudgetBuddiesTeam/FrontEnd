14bb6f652e6ed89076c9a731f45b1da499520bdd
won <andrew112517@naver.com>
2024-07-24 22:16:36 +0900

    [Feat]tableView추가

--- BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/CalendarViewController.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/CalendarViewController.swift
@@ -6,12 +6,68 @@
 //
 
 import UIKit
+import SnapKit
 
-class CalendarViewController: UIViewController {
+final class CalendarViewController: UIViewController {
+    // MARK: - Properties
+    lazy var tableView = UITableView()
 
+    // MARK: - Life Cycle ⭐️
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = BudgetBuddiesAsset.AppColor.background.color
+        
+        setupNavigationBar()
+        setupTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setupNavigationBar()
+    }
+    
+    // MARK: - Set up NavigationBar
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Set up TableView
+    private func setupTableView() {
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = true
+        tableView.showsVerticalScrollIndicator = false
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.view.addSubview(tableView)
+        
+        // 제약조건
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+    }
+}
 
-        self.view.backgroundColor = .red
+// MARK: - UITableView DataSource
+extension CalendarViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - UITableView Delegate
+extension CalendarViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
 }

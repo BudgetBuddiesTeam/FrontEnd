0dcc009d482b7480ea51c457952deae3fad1606c
won <andrew112517@naver.com>
2024-07-24 22:02:10 +0900

    [Feat]기존 폴더 삭제, CalendarViewController새로 추가

---
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/CalendarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewController.swift
+//  BudgetBuddies
+//
+//  Created by 김승원 on 7/24/24.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+}

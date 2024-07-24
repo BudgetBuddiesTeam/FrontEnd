77bece587faeed52bf2040c8e8cea633ffa72e0d
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-07-24 15:29:01 +0900

    [Chore] Info.plist 파일 추적

--- BudgetBuddies/Project.swift
+++ BudgetBuddies/Project.swift
@@ -8,20 +8,6 @@ let project = Project(
       destinations: .iOS,
       product: .app,
       bundleId: "com.budgetBuddies.app",
-      infoPlist: .extendingDefault(with: [
-        "UILaunchStoryboardName": "LaunchScreen.storyboard",
-        "UIApplicationSceneManifest": [
-          "UIApplicationSupportsMultipleScenes": false,
-          "UISceneConfigurations": [
-            "UIWindowSceneSessionRoleApplication": [
-              [
-                "UISceneConfigurationName": "Default Configuration",
-                "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate",
-              ]
-            ]
-          ],
-        ],
-      ]),
       sources: ["BudgetBuddies/Sources/**"],
       resources: ["BudgetBuddies/Resources/**"],
       dependencies: [

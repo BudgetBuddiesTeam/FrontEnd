a70eeb20b65d94752e9653f5078e011d5598052e
won <andrew112517@naver.com>
2024-07-26 22:08:26 +0900

    [Feat]InfoListViewController추가

---
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/InfoList/Controllers/InfoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoListViewController.swift
+//  BudgetBuddies
+//
+//  Created by 김승원 on 7/26/24.
+//
+
+import UIKit
+
+class InfoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+}

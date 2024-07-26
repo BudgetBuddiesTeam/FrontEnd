8ce14e2d9def294a3b44273ed0f61fb691483e2f
won <andrew112517@naver.com>
2024-07-26 17:31:18 +0900

    [Refactor]Swift-Format

--- BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/Views/CustomInfoColor.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/Views/CustomInfoColor.swift
@@ -9,76 +9,76 @@ import UIKit
 
 class CustomInfoColor: UIView {
 
-    enum InfoType {
-        case discount
-        case support
+  enum InfoType {
+    case discount
+    case support
+  }
+
+  // MARK: - Properties
+  let colorView: UIView = {
+    let view = UIView()
+    view.layer.masksToBounds = true
+    view.layer.cornerRadius = 4
+    return view
+  }()
+
+  let infoLabel: UILabel = {
+    let lb = UILabel()
+    lb.font = BudgetBuddiesFontFamily.Pretendard.regular.font(size: 12)
+    lb.setCharacterSpacing(-0.3)
+    lb.textColor = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1)
+    return lb
+  }()
+
+  lazy var stackView: UIStackView = {
+    let sv = UIStackView(arrangedSubviews: [colorView, infoLabel])
+    sv.axis = .horizontal
+    sv.spacing = 7
+    sv.alignment = .fill
+    sv.distribution = .fill
+    return sv
+  }()
+
+  // MARK: - override init
+  init(infoType: InfoType) {
+    super.init(frame: .zero)
+
+    switch infoType {
+    case .discount:
+      self.colorView.backgroundColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
+      self.infoLabel.text = "할인정보"
+    case .support:
+      self.colorView.backgroundColor = BudgetBuddiesAsset.AppColor.coreBlue.color
+      self.infoLabel.text = "지원정보"
     }
-    
-    // MARK: - Properties
-    let colorView: UIView = {
-        let view = UIView()
-        view.layer.masksToBounds = true
-        view.layer.cornerRadius = 4
-        return view
-    }()
-    
-    let infoLabel: UILabel = {
-        let lb = UILabel()
-        lb.font = BudgetBuddiesFontFamily.Pretendard.regular.font(size: 12)
-        lb.setCharacterSpacing(-0.3)
-        lb.textColor = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1)
-        return lb
-    }()
-    
-    lazy var stackView: UIStackView = {
-        let sv = UIStackView(arrangedSubviews: [colorView, infoLabel])
-        sv.axis = .horizontal
-        sv.spacing = 7
-        sv.alignment = .fill
-        sv.distribution = .fill
-        return sv
-    }()
-    
-    // MARK: - override init
-    init(infoType: InfoType) {
-        super.init(frame: .zero)
-        
-        switch infoType {
-        case .discount:
-            self.colorView.backgroundColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
-            self.infoLabel.text = "할인정보"
-        case .support:
-            self.colorView.backgroundColor = BudgetBuddiesAsset.AppColor.coreBlue.color
-            self.infoLabel.text = "지원정보"
-        }
-        
-        setupUI()
+
+    setupUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Set up UI
+  private func setupUI() {
+    self.backgroundColor = .clear
+    self.addSubview(stackView)
+
+    setupConstraints()
+  }
+
+  // MARK: - Set up Constraints
+  private func setupConstraints() {
+    colorView.snp.makeConstraints { make in
+      make.height.width.equalTo(15)
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    infoLabel.snp.makeConstraints { make in
+      make.height.equalTo(15)
     }
-    
-    // MARK: - Set up UI
-    private func setupUI() {
-        self.backgroundColor = .clear
-        self.addSubview(stackView)
-        
-        setupConstraints()
+
+    stackView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
     }
-    
-    // MARK: - Set up Constraints
-    private func setupConstraints() {
-        colorView.snp.makeConstraints { make in
-            make.height.width.equalTo(15)
-        }
-        
-        infoLabel.snp.makeConstraints { make in
-            make.height.equalTo(15)
-        }
-        
-        stackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
+  }
 }
--- BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/Views/MainCalendarCell.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Screen/Calendar/Calendar/Views/MainCalendarCell.swift
@@ -8,72 +8,71 @@
 import UIKit
 
 class MainCalendarCell: UITableViewCell {
-    // MARK: - Properties
-    static let identifier = "MainCalendarCell"
+  // MARK: - Properties
+  static let identifier = "MainCalendarCell"
 
-    // MARK: - UI Components
-    // 뒷 배경
-    var backView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 15
-        
-        view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
-        view.layer.shadowOpacity = 1
-        view.layer.shadowRadius = 5 //반경
-        view.layer.shadowOffset = CGSize(width: 0, height: 0) //위치조정
-        view.layer.masksToBounds = false //
-        return view
-    }()
-    
+  // MARK: - UI Components
+  // 뒷 배경
+  var backView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .white
+    view.layer.cornerRadius = 15
+
+    view.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+    view.layer.shadowOpacity = 1
+    view.layer.shadowRadius = 5  //반경
+    view.layer.shadowOffset = CGSize(width: 0, height: 0)  //위치조정
+    view.layer.masksToBounds = false  //
+    return view
+  }()
+
+  // 할인정보, 지원정보
+  var discountInfo = CustomInfoColor(infoType: .discount)
+  var supportInfo = CustomInfoColor(infoType: .support)
+
+  lazy var infoStackView: UIStackView = {
+    let sv = UIStackView(arrangedSubviews: [discountInfo, supportInfo])
+    sv.axis = .horizontal
+    sv.spacing = 11
+    sv.alignment = .center
+    sv.distribution = .fill
+    return sv
+  }()
+
+  // MARK: - init
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: .default, reuseIdentifier: reuseIdentifier)
+
+    setupUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Set up UI
+  private func setupUI() {
+    self.backgroundColor = .clear
+    self.addSubviews(backView)
+    self.contentView.addSubviews(infoStackView)
+
+    setupConstraints()
+  }
+
+  // MARK: - Set up Constraints
+  private func setupConstraints() {
+    // 캘린더 백뷰
+    backView.snp.makeConstraints { make in
+      make.top.equalToSuperview()
+      make.leading.trailing.equalToSuperview().inset(16)
+      make.height.equalTo(510)
+    }
+
     // 할인정보, 지원정보
-    var discountInfo = CustomInfoColor(infoType: .discount)
-    var supportInfo = CustomInfoColor(infoType: .support)
-    
-    lazy var infoStackView: UIStackView = {
-        let sv = UIStackView(arrangedSubviews: [discountInfo, supportInfo])
-        sv.axis = .horizontal
-        sv.spacing = 11
-        sv.alignment = .center
-        sv.distribution = .fill
-        return sv
-    }()
-    
-    
-    // MARK: - init
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .default, reuseIdentifier: reuseIdentifier)
-        
-        setupUI()
+    infoStackView.snp.makeConstraints { make in
+      make.top.equalTo(backView.snp.bottom).offset(7)
+      make.leading.equalTo(backView.snp.leading).inset(11)
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - Set up UI
-    private func setupUI() {
-        self.backgroundColor = .clear
-        self.addSubviews(backView)
-        self.contentView.addSubviews(infoStackView)
-        
-        setupConstraints()
-    }
-    
-    // MARK: - Set up Constraints
-    private func setupConstraints() {
-        // 캘린더 백뷰
-        backView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.trailing.equalToSuperview().inset(16)
-            make.height.equalTo(510)
-        }
-        
-        // 할인정보, 지원정보
-        infoStackView.snp.makeConstraints { make in
-            make.top.equalTo(backView.snp.bottom).offset(7)
-            make.leading.equalTo(backView.snp.leading).inset(11)
-        }
-        
-    }
+
+  }
 }

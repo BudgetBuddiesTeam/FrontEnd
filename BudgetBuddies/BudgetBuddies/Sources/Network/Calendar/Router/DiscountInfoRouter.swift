43b8f405470580c929a9d390cdb7fdf81419b928
won <andrew112517@naver.com>
2024-08-09 17:00:20 +0900

    [Feat] DiscountsInfoRouter 생성

--- BudgetBuddies/BudgetBuddies/Sources/Network/Calendar/Router/DiscountInfoRouter.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Network/Calendar/Router/DiscountInfoRouter.swift
@@ -12,6 +12,44 @@ enum DiscountInfoRouter {
     case getDiscounts(request: InfoRequest)
 }
 
-//extension DiscountInfoRouter: TargetType {
-//    
-//}
+extension DiscountInfoRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: ServerInfo.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getDiscounts:
+            return "/discounts"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getDiscounts:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getDiscounts(let request):
+            let parameters: [String: Any] = [
+                "year": request.year,
+                "month": request.month,
+                "page": request.page,
+                "size": request.size
+            ]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .getDiscounts:
+            return ["Content-type": "application/json"]
+        }
+    }
+    
+    
+}

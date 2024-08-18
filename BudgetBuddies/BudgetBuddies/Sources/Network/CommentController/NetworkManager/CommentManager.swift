789ad97b9b6360d6aff20ad868a9af349ae81841
won <andrew112517@naver.com>
2024-08-18 17:07:28 +0900

    [Feat] 댓글 하나 가져오기 매니저 설계

--- BudgetBuddies/BudgetBuddies/Sources/Network/CommentController/NetworkManager/CommentManager.swift
+++ BudgetBuddies/BudgetBuddies/Sources/Network/CommentController/NetworkManager/CommentManager.swift
@@ -15,10 +15,17 @@ final class CommentManager {
     let CommentProvider = MoyaProvider<CommentRouter>()
     
     // Result타입 선언
+    // 전체 댓글 가져오기
     typealias DiscountsCommentsNetworkCompletion = (Result<DiscountsCommentsResponseDTO, Error>) -> Void
     typealias SupportsCommentsNetworkCompletion = (Result<SupportsCommentsResponseDTO, Error>) -> Void
-    typealias postCommentsNetworkCompletion = (Result<Response, Error>) -> Void
-    typealias deleteCommentsNetworkCompletion = (Result<Response, Error>) -> Void
+    
+    // 댓글 등록, 삭제
+    typealias PostCommentsNetworkCompletion = (Result<Response, Error>) -> Void
+    typealias DeleteCommentsNetworkCompletion = (Result<Response, Error>) -> Void
+    
+    // 댓글 하나 가져오기
+    typealias GetOneDiscountsCommentsNetworkCompletion = (Result<GetOneDiscountsCommentsResponseDTO, Error>) -> Void
+    typealias GetOneSupportsCommentsNetworkCompletion = (Result<GetOneSupportsCommentsResponseDTO, Error>) -> Void
     
     // MARK: - 할인정보 전체 댓글 불러오기
     func fetchDiscountsComments(discountInfoId: Int, request: CommentRequest, completion: @escaping(DiscountsCommentsNetworkCompletion)) {
@@ -68,7 +75,7 @@ final class CommentManager {
     
     // MARK: - 할인정보 댓글 쓰기
     // 여기 설계하면서 느낀 건데, post는 굳이 매니저로 관리하지 않아도 될 거 같은 느낌이 듭니다.
-    func postDiscountsComments(userId: Int, request: DiscountsCommentsRequestDTO, completion: @escaping(postCommentsNetworkCompletion)) {
+    func postDiscountsComments(userId: Int, request: DiscountsCommentsRequestDTO, completion: @escaping(PostCommentsNetworkCompletion)) {
         CommentProvider.request(.addDiscountsComments(userId: userId, request: request)) { result in
             switch result {
             case .success(let response):
@@ -83,7 +90,7 @@ final class CommentManager {
     }
     
     // MARK: - 지원정보 댓글 쓰기
-    func postSupportsComments(userId: Int, request: SupportsCommentsRequestDTO, completion: @escaping(postCommentsNetworkCompletion)) {
+    func postSupportsComments(userId: Int, request: SupportsCommentsRequestDTO, completion: @escaping(PostCommentsNetworkCompletion)) {
         CommentProvider.request(.addSupportsComments(userId: userId, request: request)) { result in
             switch result {
             case .success(let response):
@@ -98,7 +105,7 @@ final class CommentManager {
     }
     
     // MARK: - 댓글 삭제
-    func deleteComments(commentId: Int, completion: @escaping(deleteCommentsNetworkCompletion)) {
+    func deleteComments(commentId: Int, completion: @escaping(DeleteCommentsNetworkCompletion)) {
         CommentProvider.request(.deleteComments(commentId: commentId)) { result in
             switch result {
             case .success(let response):
@@ -110,5 +117,52 @@ final class CommentManager {
                 completion(.failure(error))
             }
         }
+    }
+    
+    // MARK: - 할인정보 댓글 하나 가져오기
+    func getOneDiscountsComments(commentId: Int, completion: @escaping(GetOneDiscountsCommentsNetworkCompletion)) {
+        CommentProvider.request(.getOneDiscountsComments(commentId: commentId)) { result in
+            switch result {
+            case .success(let response):
+                print("통신 성공.... 데이터 디코딩 시작")
+                do {
+                    let decoder = JSONDecoder()
+                    let comment = try decoder.decode(GetOneDiscountsCommentsResponseDTO.self, from: response.data)
+                    completion(.success(comment))
+                    
+                } catch {
+                    print("데이터 디코딩 실패")
+                    completion(.failure(error))
+                }
+                
+            case .failure(let error):
+                print("통신 에레 발생")
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // MARK: - 지원정보 댓글 하나 가져오기
+    func getOneSupportsComments(commentId: Int, completion: @escaping(GetOneSupportsCommentsNetworkCompletion)) {
+        CommentProvider.request(.getOneSupportsComments(commentId: commentId)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let decoder = JSONDecoder()
+                    let comment = try decoder.decode(GetOneSupportsCommentsResponseDTO.self, from: response.data)
+                    completion(.success(comment))
+                    
+                } catch {
+                    print("데이터 디코딩 실패")
+                    completion(.failure(error))
+                }
+                
+            case .failure(let error):
+                print("통신 에러 발생")
+                completion(.failure(error))
+                
+            }
+        }
+        
     }
 }

84527269e1b33f38a46c7faf761aac78687d353c
won <andrew112517@naver.com>
2024-08-20 01:33:27 +0900

    []

---
+++ BudgetBuddies/BudgetBuddies/Sources/Network/CommentController/Requests/PutCommentRequest.swift
@@ -0,0 +1,13 @@
+//
+//  PutCommentRequest.swift
+//  BudgetBuddies
+//
+//  Created by 김승원 on 8/20/24.
+//
+
+import Foundation
+
+struct PutCommentRequest {
+    let content: String
+    let commentId: Int
+}

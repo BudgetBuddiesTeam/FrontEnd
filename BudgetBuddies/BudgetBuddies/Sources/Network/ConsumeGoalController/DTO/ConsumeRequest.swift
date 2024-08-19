715783f24dff00159cb55cca9d45065e75c30b45
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-08-19 16:12:00 +0900

    [Feat] ConsumeRequest 추가

---
+++ BudgetBuddies/BudgetBuddies/Sources/Network/ConsumeGoalController/DTO/ConsumeRequest.swift
@@ -0,0 +1,20 @@
+//
+//  ConsumeRequest.swift
+//  BudgetBuddies
+//
+//  Created by 이승진 on 8/11/24.
+//
+
+import Foundation
+
+// MARK: - ConsumeRequest
+struct ConsumeRequest: Codable {
+    let consumptionGoalList: [ConsumptionGoalList]
+}
+
+// MARK: - ConsumptionGoalList
+struct ConsumptionGoalList: Codable {
+    let categoryId, goalAmount: Int
+}
+
+

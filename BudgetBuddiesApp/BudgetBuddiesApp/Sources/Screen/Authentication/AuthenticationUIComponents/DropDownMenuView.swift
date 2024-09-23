32d1cf92da49b8ffdf955907f4e131ed63ef11c4
won <andrew112517@naver.com>
2024-09-23 21:21:37 +0900

    [Feat] dropDownMenuView 컴포넌트 생성

---
+++ BudgetBuddiesApp/BudgetBuddiesApp/Sources/Screen/Authentication/AuthenticationUIComponents/DropDownMenuView.swift
@@ -0,0 +1,70 @@
+//
+//  DropDownMenuView.swift
+//  BudgetBuddiesApp
+//
+//  Created by 김승원 on 9/23/24.
+//
+
+import UIKit
+import SnapKit
+
+class DropDownMenuView: UIView {
+    // MARK: - UI Components
+    let regionLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "거주지역"
+        lb.font = BudgetBuddiesAppFontFamily.Pretendard.regular.font(size: 16)
+        lb.textColor = UIColor(red: 0.72, green: 0.72, blue: 0.72, alpha: 1)
+        lb.setCharacterSpacing(-0.4)
+        lb.textAlignment = .left
+        return lb
+    }()
+    
+    let chevronDownImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "chevronDownImage")
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set up UI
+    private func setupUI() {
+        self.backgroundColor = .clear
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 12
+        self.layer.borderColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1).cgColor // 에셋 등록 필요
+        self.layer.masksToBounds = true
+        
+        self.addSubviews(regionLabel, chevronDownImageView)
+        
+        setupConstraints()
+    }
+    
+    // MARK: - Set up Constraints
+    private func setupConstraints() {
+        regionLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(20)
+        }
+        
+        chevronDownImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(21)
+            make.centerY.equalToSuperview()
+            make.height.equalTo(18)
+            make.width.equalTo(13)
+        }
+    }
+}
--- BudgetBuddiesApp/BudgetBuddiesApp/Sources/Screen/Authentication/Join/AdditionalInformation/AdditionalInformationView.swift
+++ BudgetBuddiesApp/BudgetBuddiesApp/Sources/Screen/Authentication/Join/AdditionalInformation/AdditionalInformationView.swift
@@ -13,6 +13,39 @@ class AdditionalInformationView: UIView {
     let contentView = UIView()
     let scrollView = UIScrollView()
     
+    let stepDot = StepDotView(steps: .thirdStep)
+    
+    // 추가정보를 입력하시면 맞춤정보를 받아보실 수 있어요
+    let bigTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "추가정보를 입력하시면\n맞춤정보를 받아보실 수 있어요"
+        lb.textColor = BudgetBuddiesAppAsset.AppColor.textBlack.color
+        lb.font = BudgetBuddiesAppFontFamily.Pretendard.semiBold.font(size: 24)
+        lb.numberOfLines = 0
+        lb.textAlignment = .left
+        lb.setCharacterAndLineSpacing(characterSpacing: -0.6, lineSpacing: 0.0, lineHeightMultiple: 1.26)
+        return lb
+    }()
+    
+    let subTitleLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "나에게 꼭 맞는 할인/지원 정보를 받아보세요"
+        lb.textColor = BudgetBuddiesAppAsset.AppColor.subGray.color
+        lb.font = BudgetBuddiesAppFontFamily.Pretendard.regular.font(size: 14)
+        lb.setCharacterSpacing(-0.35)
+        return lb
+    }()
+    
+    lazy var titleStackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [bigTitleLabel, subTitleLabel])
+        sv.axis = .vertical
+        sv.spacing = 9
+        sv.alignment = .leading
+        sv.distribution = .fill
+        return sv
+    }()
+    
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,11 +64,17 @@ class AdditionalInformationView: UIView {
         self.addSubviews(scrollView)
         scrollView.addSubview(contentView)
         
+        contentView.addSubviews(stepDot, titleStackView)
+        
         setupConstraints()
     }
     
     // MARK: - Set up Constraints
     private func setupConstraints() {
+        let bigTitleHeight = 72 + 1 // 여유값 + 1
+        let subTitleHeight = 21
+        let titleStackHeight = bigTitleHeight + subTitleHeight + 9
+        
         scrollView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
@@ -47,6 +86,25 @@ class AdditionalInformationView: UIView {
             make.edges.equalToSuperview()
             make.width.equalTo(scrollView.snp.width)
             make.height.equalTo(1000)
+        }
+        
+        stepDot.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalTo(32)
+        }
+        
+        bigTitleLabel.snp.makeConstraints { make in
+            make.height.equalTo(bigTitleHeight)
+        }
+        
+        subTitleLabel.snp.makeConstraints { make in
+            make.height.equalTo(subTitleHeight)
+        }
+        
+        titleStackView.snp.makeConstraints { make in
+            make.height.equalTo(titleStackHeight)
+            make.top.equalTo(stepDot.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(16)
         }
     }
 }
